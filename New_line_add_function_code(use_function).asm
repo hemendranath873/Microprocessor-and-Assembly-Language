9e37da5bba812ce7653fb45e6097b8641131c640
hemendranath873 <hemendranath.nubtk@gmail.com>
2025-05-02 12:05:35 +0600

    first commit

---
+++ New_line_add_function_code(use_function).asm
@@ -0,0 +1,24 @@
+INCLUDE "EMU8086.INC"
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+
+MAIN PROC
+    MOV AH,1
+    INT 21H
+    MOV BL,AL 
+    
+    PRINTN ""
+    
+    
+    MOV AH,2
+    MOV DL,BL
+    INT 21H 
+    
+    MOV AH,4CH
+    INT 21H
+    
+    
+    MAIN ENDP
+END MAIN
